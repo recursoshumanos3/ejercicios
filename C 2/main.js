cbc6ea6e05debd362843d47d4ed87af6b5102bc1
alumno <alumno@gmail.com>
2019-04-08 16:13:31 +0100

    8-4-2019

--- C 2/main.js
+++ C 2/main.js
@@ -1,123 +1,99 @@
+function comenzar(){
+    const entrada = document.querySelector(".begin");
+    const salida = document.querySelector(".finish");
+    const listado = ['D','A','M','I','C'];
+    salida.value ="";
+    let ganancias = parser(entrada,listado);
+    let myms = ganancias.map(dia => findMinMax(dia));
+    console.log(myms);
+    let esMayorComidas = ganancias.map(dia => mayor(dia));
+    console.log(esMayorComidas);
+    salida.value = imprimir(myms,esMayorComidas);
+}
 
-function parser(e) {
-    let ganancias = [{}];
-    let N = 0;
+function parser(e,listado) {
+    let ganancias = [{}],N = 0;
     e.value.split('\n').map(linea => { 
         const lineaA = linea.split(' '); 
-        if(["D","M","A","C","I"].includes(lineaA[0])) { 
-            if(ganancias[N][lineaA[0]]===undefined) 
-                ganancias[N][lineaA[0]]=0 
+        if(listado.includes(lineaA[0])) { 
+            if(ganancias[N][lineaA[0]]===undefined){ganancias[N][lineaA[0]]=0;} 
             ganancias[N][lineaA[0]] += parseFloat(lineaA[1]);
         } else if (lineaA[0] === 'N') { 
+            ganancias[N] = construir(ganancias[N],listado);
             N += 1 
             ganancias[N] = {}; 
-        } 
+        }else{ganancias[N] = construir(ganancias[N],listado);} 
     });
+    console.log(ganancias);
     return ganancias
 }
-function findMinMax(o) {
-    let min = 99999, max = 0;
-    let keyMin = undefined, keyMax = undefined
-    Object.keys(o).map(key => {
-        let v = o[key]
 
+function construir(o,l) {
+   l = l.map(el => o[el]===undefined ? o[el] = 0:o[el]);
+    return o;
+}
+
+function findMinMax(o) {
+    let min = 99999, max = 0, keyMin = undefined, keyMax = undefined, x=0,y=0;
+    Object.keys(o).map(key => {
+        let v = o[key];
         keyMin = (v < min) ? key : keyMin;
         min = (v < min) ? v : min;
 
         keyMax = (v > max) ? key : keyMax;
         max = (v > max) ? v : max;
     });
-    return [keyMin, keyMax];
+    Object.values(o).map(ve => ve===min ? x++:"");
+    Object.values(o).map(ve => ve===max ? y++:""); 
+    x>1 ? keyMin = "Q":"";
+    y>1 ? keyMax = "Q":"";   
+    return [keyMax ,keyMin];
   }
 
-  function esMayorComida(o) {
-    if(Object.keys(o).length === 0)
-        return
-    let totalNoComida = Object.keys(o)
-        .filter(key => key !== 'A')
-        .reduce((a,b) => a+o[b])
+  function mayor(o) {
+    if(Object.keys(o).length === 0){return}
+    let totalNoComida = Object.keys(o).filter(key => key !== 'A')
+                                        .map(a => a = o[a])
+                                        .reduce((c,b) => c+b);
+    //console.log(totalNoComida);
     return o['A'] === undefined ? false : o['A'] > totalNoComida
   }
 
-function traducir(e,s,l){
-    e = e.value.split("\n");
-
-    e.pop();
-    while (e.includes("N 0")) {
-        procesar(e.splice(0,e.indexOf("N 0")),s,l);
-        e.shift();
-    }
-    procesar(e,s,l);
-}
-
-function procesar(dia,s,l) {
-  //  console.log(dia);
-    l =  l.map(el => {
-        let contador = 0;
-        dia.map(el2 =>{
-            if(el2.substring(0,1)===el){contador += parseFloat(el2.substring(2,el2.length));};
-        });
-        const resultado = el + " " + contador;
-        return resultado;
+  function imprimir(a,b) {
+    let resultado = "";
+    //console.log(a,"---",b);
+    a.map((el,i) => {
+        el.map(el2 => resultado += salida(el2));
+        resultado += salida(b[i]) + "\n";
     });
-//    console.log(l);
-    imprimir(l,s);
-}
+    return resultado;
+  }
 
-function imprimir(l,s) {
-    const numeral = l.map(el => {return parseFloat(el.substring(2,el.length))});
-    const letral = l.map(el => {return el.substring(0,1);});
-    salir(numeral,letral,s);
-}
-
-function salir(n,l,s) {
-   const max = Math.max(...n);
-   n.indexOf(max) !== n.lastIndexOf(max) ? s.value += "EMPATE#":s.value += elegir(l[n.indexOf(max)]);
-   const min = Math.min(...n);
-   n.indexOf(min) !== n.lastIndexOf(min) ? s.value += "EMPATE#":s.value += elegir(l[n.indexOf(min)]);
-   n[0] > n.reduce(function(d , el) {return d + el})/n.length ? s.value += "SI\n":s.value += "NO\n";
-}
-
-function elegir(l){
-    switch (l) {
-        case "A":
-            l = "Comidas#";
+  function salida(a) {
+      switch (a) {
+          case "D": a = "DESAYUNO#";
+            break;
+  
+          case "M": a = "MERIENDAS#"; 
             break;
 
-        case "C":
-            l = "Copas#";
+          case "A": a = "COMIDAS#";
             break;
 
-        case "D":
-            l = "Desayuno#";
+          case "C": a = "COPAS#";   
             break;
 
-        case "M":
-            l = "Meriendas#";
-            break;
-            
-        case "I":
-            l = "Cenas#";
+          case "I": a = "CENAS#";
             break;
 
-        default:
-            l = "desconocido#";
+          case "Q": a = "EMPATE#";
             break;
-    }
-    return l;
-}
 
+          case true: a = "SI";
+            break;
 
-function comenzar(){
-    const entrada = document.querySelector(".begin");
-    const salida = document.querySelector(".finish");
-    const listado = ["A","C","D","M","I"];
-    salida.value ="";
-    //traducir(entrada,salida,listado); // parser
-    let ganancias = parser(entrada)
-    let myms = ganancias.map(dia => findMinMax(dia));
-    console.log(myms);
-    let esMayorComidas = ganancias.map(dia => esMayorComida(dia));
-    console.log(esMayorComidas);
-
-}
+          case false: a = "NO";
+            break;
+      }
+    return a;
+  }
